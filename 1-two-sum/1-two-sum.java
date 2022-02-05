ca3c19b6645c22ddbfa6d49bda41fa8c90d1e7b0
Janhavi Patil <91064646+janhavipatil23@users.noreply.github.com>
2022-02-04 21:08:23 -0500

    Time: 3 ms (76.65%), Space: 45.9 MB (5.25%) - LeetHub

---
+++ 1-two-sum/1-two-sum.java
@@ -0,0 +1,17 @@
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i=0; i<nums.length; i++){
+            int diff = target - nums[i];
+            
+            if(map.containsKey(diff)){
+                return new int[] {i,map.get(diff)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
