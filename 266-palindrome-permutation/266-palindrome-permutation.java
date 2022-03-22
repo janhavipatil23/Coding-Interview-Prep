e2c992bb4a14dabdc4ed45ebc5448fd8f6e45994
Janhavi Patil <91064646+janhavipatil23@users.noreply.github.com>
2022-03-22 09:54:47 -0400

    Time: 1 ms (49.84%), Space: 40.3 MB (72.47%) - LeetHub

--- 266-palindrome-permutation/266-palindrome-permutation.java
+++ 266-palindrome-permutation/266-palindrome-permutation.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean canPermutePalindrome(String s) {
-        int count = 0;
-        for (char i = 0; i < 128 && count <= 1; i++) {
-            int ct = 0;
-            for (int j = 0; j < s.length(); j++) {
-                if (s.charAt(j) == i)
-                    ct++;
-            }
-            count += ct % 2;
-        }
-        return count <= 1;
+        // int count = 0;
+        // for (char i = 0; i < 128 && count <= 1; i++) {
+        //     int ct = 0;
+        //     for (int j = 0; j < s.length(); j++) {
+        //         if (s.charAt(j) == i)
+        //             ct++;
+        //     }
+        //     count += ct % 2;
+        // }
+        // return count <= 1;
+        
+     HashMap < Character, Integer > map = new HashMap < > ();
+     for (int i = 0; i < s.length(); i++) {
+         map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+     }
+     int count = 0;
+     for (char key: map.keySet()) {
+         count += map.get(key) % 2;
+     }
+     return count <= 1;
     }
 }
