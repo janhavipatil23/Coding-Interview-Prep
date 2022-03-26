f5306b86e3f1550056565015646798aaf83a574a
Janhavi Patil <91064646+janhavipatil23@users.noreply.github.com>
2022-03-26 10:17:54 -0400

    Time: 0 ms, Memory: 42.5 MB - LeetHub

---
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int dominantIndex(int[] nums) {
+        
+        int max = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+        }
+        
+        for(int j=0; j<n; j++){
+            if(nums[j] * 2 > max && nums[j] != max) {
+                return -1;
+            }
+        }
+        
+        for(int k=0; k<n; k++){
+            if(nums[k] == max){
+                return k;
+            }
+        }
+        return 0;
+        
+    
+    }
+}
