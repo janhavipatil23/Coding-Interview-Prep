775859e5be1fe2c52b27e689c28bc4c2f78a7132
Janhavi Patil <91064646+janhavipatil23@users.noreply.github.com>
2022-03-26 10:35:33 -0400

    Time: 0 ms, Memory: 39.7 MB - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -1,27 +1,49 @@
 class Solution {
     public int dominantIndex(int[] nums) {
         
-        int max = 0;
-        int n = nums.length;
-        for(int i=0; i<n; i++){
-            if(nums[i] > max){
-                max = nums[i];
+//         int max = 0;
+//         int n = nums.length;
+//         for(int i=0; i<n; i++){
+//             if(nums[i] > max){
+//                 max = nums[i];
+//             }
+//         }
+        
+//         for(int j=0; j<n; j++){
+//             if(nums[j] * 2 > max && nums[j] != max) {
+//                 return -1;
+//             }
+//         }
+        
+//         for(int k=0; k<n; k++){
+//             if(nums[k] == max){
+//                 return k;
+//             }
+//         }
+        
+//         return 0;
+        
+        int largest;
+        int second;
+        if (nums.length == 1) return 0;
+        if(nums[0] > nums[1]) {
+            largest = 0;
+            second= 1;
+        }
+        else {
+            largest = 1;
+            second = 0;
+        }
+        // if (nums.length == 1) return 0;
+        for(int i = 2; i < nums.length; i++) {
+            if (nums[i] > nums[largest]) {
+                second = largest;
+                largest = i;
+            } else if (nums[i] > nums[second]) {
+                second = i;
             }
         }
-        
-        for(int j=0; j<n; j++){
-            if(nums[j] * 2 > max && nums[j] != max) {
-                return -1;
-            }
-        }
-        
-        for(int k=0; k<n; k++){
-            if(nums[k] == max){
-                return k;
-            }
-        }
-        
-        return 0;
+        return nums[largest] >= nums[second] * 2 ? largest : -1;
     
     }
 }
